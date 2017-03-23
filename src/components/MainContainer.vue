d3ecbdec51a6aefb30246a3162f7831e917703f6
Caique Pessoa Xavier <caique.xavier@quigroup.br>
2017-03-23 16:13:40 -0300

    first commit

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mainContainer">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Mysoundtips</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainContainer',
+  data () {
+    return {
+      msg: 'Great!',
+      seen: true
+    }
+  }
+}
+</script>
---
+++ src/components/sidebar/sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="sidebar">
+    <p>{{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: sidebar
+  data(){
+    msg: 'Hey, Thats My Msg'
+  }
+
+}
+</script>
